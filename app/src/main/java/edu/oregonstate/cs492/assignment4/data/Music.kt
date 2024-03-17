3a8a423cddfb91821083ba1ff5edd6d3da113373
CollinK23 <kimballcollin@gmail.com>
2024-03-16 21:19:39 -0700

    Updated retrofit service

--- app/src/main/java/edu/oregonstate/cs492/assignment4/data/Music.kt
+++ app/src/main/java/edu/oregonstate/cs492/assignment4/data/Music.kt
@@ -1,21 +1,22 @@
 package edu.oregonstate.cs492.assignment4.data
 
 import com.squareup.moshi.FromJson
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.ToJson
 
-/**
- * This class encapsulates data about the city fetched from the OpenWeather API's 5-day/3-hour
- * forecast.  It does not directly correspond to the JSON data.  The classes below are used for
- * JSON parsing, and information from them is used by the custom JSON adapter at the bottom of
- * this file to construct this class.
- */
-data class ForecastCity(
-    val name: String,
-    val lat: Double,
-    val lon: Double,
-    val tzOffsetSec: Int
-)
+///**
+// * This class encapsulates data about the city fetched from the OpenWeather API's 5-day/3-hour
+// * forecast.  It does not directly correspond to the JSON data.  The classes below are used for
+// * JSON parsing, and information from them is used by the custom JSON adapter at the bottom of
+// * this file to construct this class.
+// */
+//data class Music(
+//    val name: String,
+//    val lat: Double,
+//    val lon: Double,
+//    val tzOffsetSec: Int
+//)
 
 /* ******************************************************************************************
  * Below is a set of classes used to parse the JSON response from the OpenWeather API into
@@ -25,42 +26,78 @@ data class ForecastCity(
  * a ForecastCity object.
  * ******************************************************************************************/
 
+
+data class MusicFormat(
+    val artist: String,
+    val songTitle: String,
+    val shortUrl: String,
+    val shareUrl: String,
+    val songImage: String,
+    val duration: Int
+)
+
 /**
  * This class represents the `city` field of the JSON response from the OpenWeather API.
  */
 @JsonClass(generateAdapter = true)
-data class OpenWeatherCityJson(
+data class Track(
+    val id: String,
     val name: String,
-    val coord: OpenWeatherCityCoordJson,
-    val timezone: Int
+    val duration: Int,
+    @Json(name = "artist_id") val artistId: String,
+    @Json(name = "artist_name") val artistName: String,
+    @Json(name = "artist_idstr") val artistIdStr: String,
+    @Json(name = "album_name") val albumName: String,
+    @Json(name = "album_id") val albumId: String,
+    @Json(name = "license_ccurl") val licenseCcurl: String,
+    val position: Int,
+    val releaseDate: String,
+    @Json(name = "album_image") val albumImage: String,
+    val audio: String,
+    @Json(name = "audiodownload") val audioDownload: String,
+    @Json(name = "prourl") val proUrl: String,
+    @Json(name = "shorturl") val shortUrl: String,
+    @Json(name = "shareurl") val shareUrl: String,
+    val waveform: String,
+    val image: String,
+    @Json(name = "audiodownload_allowed") val audioDownloadAllowed: Boolean
 )
 
-/**
- * This class represents the `city.coord` field of the JSON response from the OpenWeather API.
- */
 @JsonClass(generateAdapter = true)
-data class OpenWeatherCityCoordJson(
-    val lat: Double,
-    val lon: Double
+data class Headers(
+    val status: String,
+    val code: Int,
+    @Json(name = "error_message") val errorMessage: String,
+    val warnings: String,
+    @Json(name = "results_count") val resultsCount: Int,
+    val next: String
 )
 
-/**
- * This class is a custom JSON adapter for use with Moshi.  It uses the classes above to represent
- * and parse the JSON response from the OpenWeather API, then it takes data from the parsed JSON
- * and uses it to build a [ForecastCity] object, which becomes the ultimate return value from the
- * JSON parsing.
- */
-class OpenWeatherCityJsonAdapter {
+@JsonClass(generateAdapter = true)
+data class MusicJson(
+    @Json(name = "headers") val headers: Headers,
+    @Json(name = "results") val results: MusicResults
+)
+
+@JsonClass(generateAdapter = true)
+data class MusicResults(
+    @Json(name = "tracks") val tracks: List<Track>
+)
+
+
+class MusicJsonAdapter {
     @FromJson
-    fun forecastCityFromJson(city: OpenWeatherCityJson) = ForecastCity(
-        name = city.name,
-        lat = city.coord.lat,
-        lon = city.coord.lon,
-        tzOffsetSec = city.timezone
+    fun musicFormatFromJson(list: Track) = MusicFormat(
+        artist = list.artistName,
+        songTitle = list.name,
+        shortUrl = list.shortUrl,
+        shareUrl = list.shareUrl,
+        songImage = list.image,
+        duration = list.duration
     )
 
     @ToJson
-    fun forecastCityToJson(forecastCity: ForecastCity): String {
-        throw UnsupportedOperationException("encoding ForecastCity to JSON is not supported")
+    fun musicFormatToJson(musicFormat: MusicFormat): String {
+        throw UnsupportedOperationException("encoding MusicFormat to JSON is not supported")
     }
 }
--- app/src/main/java/edu/oregonstate/cs492/assignment4/data/MusicService.kt
+++ app/src/main/java/edu/oregonstate/cs492/assignment4/data/MusicService.kt
@@ -10,7 +10,7 @@ import retrofit2.http.Query
 /**
  * This is a Retrofit service interface encapsulating communication with the OpenWeather API.
  */
-interface OpenWeatherService {
+interface MusicService {
     /**
      * This method is used to query the OpenWeather API's 5-day/3-hour forecast method:
      * https://openweathermap.org/forecast5.  This is a suspending function, so it must be called
@@ -27,52 +27,28 @@ interface OpenWeatherService {
      *   if the API call was successful.
      */
     @GET("forecast")
-    suspend fun loadFiveDayForecast(
-        @Query("q") location: String?,
-        @Query("units") units: String?,
-        @Query("appid") apiKey: String
-    ) : Response<FiveDayForecast>
-
-    /**
-     * This method is used to query the OpenWeather API's current weather method:
-     * https://openweathermap.org/current.  This is a suspending function, so it must be called
-     * in a coroutine or within another suspending function.
-     *
-     * @param location Specifies the location for which to fetch weather data.  For US cities,
-     *   this should be specified as "<city>,<state>,<country>" (e.g. "Corvallis,OR,US"), while
-     *   for international cities, it should be specified as "<city>,<country>" (e.g. "London,GB").
-     * @param units Specifies the type of units that should be returned by the OpenWeather API.
-     *   Can be one of: "standard", "metric", and "imperial".
-     * @param apiKey Should be a valid OpenWeather API key.
-     *
-     * @return Returns a Retrofit `Response<>` object that will contain a [ForecastPeriod] object
-     *   if the API call was successful.
-     */
-    @GET("weather")
-    suspend fun loadCurrentWeather(
-        @Query("q") location: String?,
-        @Query("units") units: String?,
-        @Query("appid") apiKey: String
-    ) : Response<ForecastPeriod>
+    suspend fun loadMusic(
+        @Query("client_id") clientId: String,
+        @Query("tags") tags: String?
+    ) : Response<MusicResults>
 
     companion object {
-        private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+        private const val BASE_URL = "https://api.jamendo.com/v3.0/tracks/"
 
         /**
          * This method can be called as `OpenWeatherService.create()` to create an object
          * implementing the OpenWeatherService interface and which can be used to make calls to
          * the OpenWeather API.
          */
-        fun create() : OpenWeatherService {
+        fun create() : MusicService {
             val moshi = Moshi.Builder()
-                .add(OpenWeatherForecastPeriodJsonAdapter())
-                .add(OpenWeatherCityJsonAdapter())
+                .add(MusicJsonAdapter())
                 .build()
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create(moshi))
                 .build()
-                .create(OpenWeatherService::class.java)
+                .create(MusicService::class.java)
         }
     }
 }
