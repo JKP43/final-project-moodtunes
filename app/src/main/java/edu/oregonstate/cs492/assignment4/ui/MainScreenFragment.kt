cb1bb78fcb44190297be0b46681b583736149964
JKP43 <parkjin@oregonstate.edu>
2024-03-19 15:19:19 -0700

    Fixed findViewById

--- app/src/main/java/edu/oregonstate/cs492/assignment4/ui/MainScreenFragment.kt
+++ app/src/main/java/edu/oregonstate/cs492/assignment4/ui/MainScreenFragment.kt
@@ -2,20 +2,19 @@ package edu.oregonstate.cs492.assignment4.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import edu.oregonstate.cs492.assignment4.R
-import androidx.appcompat.content.res.AppCompatResources
 
-class MainScreenFragment : Fragment(R.layout.main_screen){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.main_screen)
+class MainScreenFragment : Fragment(R.layout.main_screen) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val happyButton = findViewById<Button>(R.id.happyButton)
-        val sadButton = findViewById<Button>(R.id.sadButton)
-        val angryButton = findViewById<Button>(R.id.angryButton)
-        val relaxedButton = findViewById<Button>(R.id.relaxedButton)
+        val happyButton = view.findViewById<Button>(R.id.happyButton)
+        val sadButton = view.findViewById<Button>(R.id.sadButton)
+        val angryButton = view.findViewById<Button>(R.id.angryButton)
+        val relaxedButton = view.findViewById<Button>(R.id.relaxedButton)
 
         // Set onClickListeners for each mood button
         happyButton.setOnClickListener {
@@ -37,7 +36,7 @@ class MainScreenFragment : Fragment(R.layout.main_screen){
     }
 
     private fun navigateToSongs(mood: String) {
-        val intent = intent(this, MusicActivity::class.java)
+        val intent = Intent(requireContext(), MusicActivity::class.java)
         intent.putExtra("mood", mood)
         startActivity(intent)
     }
